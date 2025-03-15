6a2387f4c88c05f38caa292dbeba3a515784d3ee
Danny Kurniawan <donatepic67@gmail.com>
2025-03-15 18:17:15 +0700

    Add Overlay Redmi Note 14 5G

--- overlay.mk
+++ overlay.mk
@@ -430,6 +430,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-xiaomi-redminote12pro5g \
 	treble-overlay-xiaomi-redminote13pro4g \
 	treble-overlay-xiaomi-redminote13pro4g-systemui \
+	treble-overlay-xiaomi-redminote145g \
 	treble-overlay-xiaomi-redminote5 \
 	treble-overlay-xiaomi-redminote6pro \
 	treble-overlay-xiaomi-redminote6pro-systemui \
